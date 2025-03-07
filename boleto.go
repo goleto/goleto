46ce4904dd81cf2c8b187abdee67dd931e35dc15
Gabriel Araújo <gabrielaraujo@stone.com.br>
2025-03-07 15:18:44 -0300

    fix: Update expiration date calculation to use October 7, 1997

--- boleto.go
+++ boleto.go
@@ -55,7 +55,7 @@ func (b Boleto) calcExpirationDateAt(now time.Time) (year int, month time.Month,
 	factor, _ := strconv.ParseInt(b.validBarcode[5:9], 10, 32)
 
 	if factor < 1000 {
-		epoch := time.Date(1997, time.August, 7, 0, 0, 0, 0, brTz)
+		epoch := time.Date(1997, time.October, 7, 0, 0, 0, 0, brTz)
 		return epoch.AddDate(0, 0, int(factor)).Date()
 	}
 
